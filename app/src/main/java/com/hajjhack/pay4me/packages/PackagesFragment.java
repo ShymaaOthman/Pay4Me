e3fec6ad16008ada961fff3ec044fa7421044e7b
Shymaa Othman <shemah2006@gmail.com>
2018-08-02 01:27:26 +0200

    refactor

--- app/src/main/java/com/hajjhack/pay4me/packages/PackagesFragment.java
+++ app/src/main/java/com/hajjhack/pay4me/packages/PackagesFragment.java
@@ -1,7 +1,5 @@
 package com.hajjhack.pay4me.packages;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,14 +8,6 @@ import android.view.ViewGroup;
 
 import com.hajjhack.pay4me.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PackagesFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PackagesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PackagesFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,8 +17,6 @@ public class PackagesFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public PackagesFragment() {
         // Required empty public constructor
@@ -68,42 +56,5 @@ public class PackagesFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_packages, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
